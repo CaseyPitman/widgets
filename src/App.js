7fa1549c49fb1afedda0a988843f2752008284a6
Casey The Great <casey@caseypitman.com>
2020-10-23 14:52:19 -0500

    Basic structure build. Major components created.

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework.",
+  },
+  {
+    title: "Will I like React?",
+    content: "Absolutely. It makes UI coding so much more fun.",
+  },
+  {
+    title: "Who's the man?",
+    content: "You the man!",
+  },
+];
+
+const App = () => {
+  return (
+    <div>
+      <br/>
+      <Accordion items={items} />
+    </div>
+  );
+};
+export default App;
