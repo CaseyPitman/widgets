7de233396df3d6b385cc9e416bd73e9e467c5614
Casey The Great <casey@caseypitman.com>
2020-10-26 17:51:05 -0500

    Fix: Translate component displaying pre-translated text. Corrected.

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -3,10 +3,21 @@ import Dropdown from "./Dropdown";
 import Convert from "./Convert";
 
 const options = [
-  { label: "Afrikaans", value: "af" },
-  { label: "Arabic", value: "ar" },
-  { label: "Hindi", value: "hi" },
   { label: "Dutch", value: "nl" },
+  { label: "English", value: "en" },
+  { label: "Finnish", value: "fi" },
+  { label: "French", value: "fr" },
+  { label: "German", value: "de" },
+  { label: "Greek", value: "el" },
+  { label: "Icelandic", value: "is" },
+  { label: "Irish", value: "ga" },
+  { label: "Italian", value: "it" },
+  { label: "Latin", value: "la" },
+  { label: "Norwegian", value: "no" },
+  { label: "Russian", value: "ru" },
+  { label: "Spanish", value: "es" },
+  { label: "Swedish", value: "sv" },
+  { label: "Welsh", value: "cy" },
 ];
 
 const Translate = () => {
@@ -36,4 +47,3 @@ const Translate = () => {
 
 export default Translate;
 
-// 
